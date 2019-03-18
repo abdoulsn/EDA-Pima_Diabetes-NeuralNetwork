e4587acf7038bb526d1a3e372c07d9790d8a77c5
Abdoualye <33933023+abdoulsn@users.noreply.github.com>
2019-03-18 16:41:23 +0100

    Import packages and data read

--- pima.py
+++ pima.py
@@ -1,1 +1,30 @@
+# This Python 3 environment
+import gc
+import os
+import logging
+import datetime
+import warnings
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import itertools 
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+#plt.style.use('seaborn-white')
+#plt.style.use('seaborn')
+import lightgbm as lgb
+from tqdm import tqdm_notebook
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.model_selection import StratifiedKFold
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import train_test_split
 
+# loading data in pandas df
+df = pd.read_csv('pima.csv')
+df.info()
+
+diabetic=df[df['Outcome']==1]  #base de donnees des diabetic
+nondiabetic=df[df['Outcome']==0] # base desnon diabetics
+df.isnull().sum()
